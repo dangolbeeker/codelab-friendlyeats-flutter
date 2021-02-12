2cc8d2cbbd1f45b1e41c960c7fcb953ef32a19b6
dangolbeeker <scottb305@outlook.com>
2021-02-12 01:14:14 -0500

    added a new document to restaurants collection

--- lib/src/model/data.dart
+++ lib/src/model/data.dart
@@ -1,17 +1,3 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import './filter.dart';
@@ -21,8 +7,16 @@ import './review.dart';
 // This is the file that Codelab users will primarily work on.
 
 Future<void> addRestaurant(Restaurant restaurant) {
-  // TODO: Complete the "Add restaurants to Firestore" step.
-  return Future.value();
+  final restaurants = FirebaseFirestore.instance.collection('restaurants');
+  return restaurants.add({
+    'avgRating': restaurant.avgRating,
+    'category': restaurant.category,
+    'city': restaurant.city,
+    'name': restaurant.name,
+    'numRatings': restaurant.numRatings,
+    'photo': restaurant.photo,
+    'price': restaurant.price,
+  });
 }
 
 Stream<QuerySnapshot> loadAllRestaurants() {
